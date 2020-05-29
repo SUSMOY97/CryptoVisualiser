487b792330dbc6bbb7ec39f0e351a409abd05c95
Susmoy <susmoymukherjee@gmail.com>
2020-05-29 18:27:47 +0530

    Pages Fixed

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import WelcomeMessage from "./WelcomeMessage"
 import ConfirmButton from './ConfirmButton';
+import Page from "../Shared/Page"
 
 
 export default function(){
     
     return(
-    <div>
+    <Page name="settings">
+    
     <WelcomeMessage/>
     <ConfirmButton/>
 
-    </div>
+    </Page>
 
     )
 }
